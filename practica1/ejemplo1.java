3bdf032e3c26f8d3a506b7cfd34d0d7120a92d76
Vicente Santiago <vicentejsp@outlook.com>
2019-10-14 17:16:12 -0500

    Practica 1 -> Ejemplo 1

---
+++ practica1/ejemplo1.java
@@ -0,0 +1,8 @@
+/** La clase mas sencilla en Java */
+ public class Ejemplo1 {
+     /** Este es el metodo de clase que arranca la apliación */
+     public static void main(String[] args) {
+         /** La siguiente linea impirme la una cadena */
+         System.out.println("Hola Mundo (ADSC)");
+     }
+ }
